a7a70362b3ae9f2b73d75c38aee9b12e4b80d785
Tiens Benade <tiens.benade@gmail.com>
2022-10-23 16:07:14 +0200

    Initial commit of domain

---
+++ Wonga.RabbitMQ.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wonga.RabbitMQ.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
